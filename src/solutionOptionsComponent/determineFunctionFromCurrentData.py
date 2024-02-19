39cd850a58dcc6dba1f0e0b2b628ee916fe23019
InfinityImperium <123785084+InfinityImperium@users.noreply.github.com>
2024-02-19 16:23:47 -0600

    Update and rename determineFunction.py to determineFunctionFromCurrentData.py

--- src/solutionOptionsComponent/determineFunctionFromCurrentData.py
+++ src/solutionOptionsComponent/determineFunctionFromCurrentData.py
@@ -2,25 +2,33 @@ from sympy import *
 from sympy import Array
 import math
 import pandas as pd
+import json
+import sys
+
+with open("dataFiles/current_user_data.json", "r") as user_data:
+  jsonUserData = json.load(user_data)
+
+with open("dataFiles/constants.json", "r") as constants:
+  jsonConstants = json.load(constants)
+
 
 sh_consumption, sh_time, wh_consumption, wh_time, ac_consumption, ac_time, refrigerators_consumption, refrigerators_time, other_consumption, other_time= symbols("sh_consumption sh_time wh_consumption wh_time ac_consumption ac_time refrigerators_consumption refrigerators_time other_consumption other_time")
 
-TOTAL_CONSUMPTION_PER_HOUSE = 76.8
+TOTAL_CONSUMPTION_PER_HOUSE = jsonConstants["Total consumption per house"]
 
+ELECTRICITY_CONVERSION_FACTOR = jsonConstants["Carbon constants"]["Electricity"]
+NATURAL_GAS_CONVERSION_FACTOR = jsonConstants["Carbon constants"]["Natural gas"]
+FUEL_OIL_OR_KEROSENE_CONVERSION_FACTOR = jsonConstants["Carbon constants"]["Fuel oil or kerosene"]
+PROPANE_CONVERSION_FACTOR = jsonConstants["Carbon constants"]["Propane"]
+WOOD_CONVERSION_FACTOR = jsonConstants["Carbon constants"]["Wood"]
 
-#https://www.eia.gov/environment/emissions/co2_vol_mass.php
-ELECTRICITY_CONVERSION_FACTOR = 379.63
-NATURAL_GAS_CONVERSION_FACTOR = 116.65
-FUEL_OIL_OR_KEROSENE_CONVERSION_FACTOR = 161.35
-PROPANE_CONVERSION_FACTOR = 138.63
-WOOD_CONVERSION_FACTOR = 2.5*116.65
 Conversion_Factors = [ ["Electricity", ELECTRICITY_CONVERSION_FACTOR], ["Natural gas", NATURAL_GAS_CONVERSION_FACTOR], ["Fuel oil or kerosone", FUEL_OIL_OR_KEROSENE_CONVERSION_FACTOR], ["Propane", PROPANE_CONVERSION_FACTOR], ["Wood", WOOD_CONVERSION_FACTOR]]
 
-SH_HOURS_PER_DAY = 8
-WH_HOURS_PER_DAY = 4
-AC_HOURS_PER_DAY = (35/60 * 14)
-REFRIGERATORS_HOURS_PER_DAY = 8
-OTHER_CONSUMPTION_HOURS_PER_DAY = 8
+SH_HOURS_PER_DAY = jsonConstants["Normal hours per day"]["Space heating"]
+WH_HOURS_PER_DAY = jsonConstants["Normal hours per day"]["Water heating"]
+AC_HOURS_PER_DAY = jsonConstants["Normal hours per day"]["Air conditioning"]
+REFRIGERATORS_HOURS_PER_DAY = jsonConstants["Normal hours per day"]["Refrigerators"]
+OTHER_CONSUMPTION_HOURS_PER_DAY = jsonConstants["Normal hours per day"]["Other"]
 
 Consumption_Hours = [SH_HOURS_PER_DAY, WH_HOURS_PER_DAY, AC_HOURS_PER_DAY, REFRIGERATORS_HOURS_PER_DAY, OTHER_CONSUMPTION_HOURS_PER_DAY] 
 
@@ -28,13 +36,13 @@ Consumption_Hours = [SH_HOURS_PER_DAY, WH_HOURS_PER_DAY, AC_HOURS_PER_DAY, REFRI
 EnergyData = pd.read_csv("ce3.1 (1).xlsx - EnergyData (1).csv")
 
 
-housing_unit_type = input("Housing Unit Type: ")
-ownership = input("Ownership: ")
-year_of_construction = input("Year of Construction: ")
-square_footage = input("Square Footage: ")
-num_of_household_members = input("Number of Household Members: ")
-household_income = input("Household Income: ") 
-payment_method = input("Payment Method: ")
+housing_unit_type = jsonUserData["Statistics"]["Housing unit type"]
+ownership = jsonUserData["Statistics"]["Ownership of housing unit"]
+year_of_construction = jsonUserData["Statistics"]["Year of construction"]
+square_footage = jsonUserData["Statistics"]["Total square footage"]
+num_of_household_members = jsonUserData["Statistics"]["Number of household members"]
+household_income = jsonUserData["Statistics"]["2020 annual household income"]
+payment_method = jsonUserData["Statistics"]["Payment method for energy bills"]
 
 statsArray = [["Housing unit type", housing_unit_type], ["Ownership of housing unit", ownership], ["Year of construction", year_of_construction], ["Total square footage", square_footage], ["Number of household members", num_of_household_members], ["2020 annual household income", household_income], ["Payment method for energy bills", payment_method]]
 
@@ -47,8 +55,8 @@ func = coefficient*(sh_consumption * sh_time + wh_consumption * wh_time + ac_con
 var = func
 
 for fuel in arrayOfFuels:
-  fuel[1] = input("Type of Fuel for " + fuel[0] + ": ") 
-  fuel[2] = int(input("Number of Hours per Day: "))
+  fuel[1] = jsonUserData["Consumption information"][fuel[0]][0]
+  fuel[2] = jsonUserData["Consumption information"][fuel[0]][1]
 
 for i in range(len(statsArray)):
   for j in EnergyData.index:
@@ -72,7 +80,16 @@ for i in range(len(arrayOfFuels)):
     if (fuel[1] == factor[0]):
       summand = summand*factor[1]
     var = var.subs(symbolsOfFuels[i], summand)
-print(var)
-print(coefficient)
-print(var/coefficient)
+
+var = str(var)
+
+with open ("dataFiles/current_user_data.json", "r") as file:
+  jsonData = json.load(file)
+  jsonData["CO2 Function"] = var
+  newData  = json.dumps(jsonData, indent = 4)
+
+with open("dataFiles/current_user_data.json", "w") as file2:
+  file2.write(newData)
+
+
   
