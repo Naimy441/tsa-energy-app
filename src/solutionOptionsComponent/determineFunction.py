9d040bf9a140cfce4e8cd6040bb1e34b8c593e21
InfinityImperium <123785084+InfinityImperium@users.noreply.github.com>
2024-02-18 21:55:43 -0600

    Update determineFunction.py

--- src/solutionOptionsComponent/determineFunction.py
+++ src/solutionOptionsComponent/determineFunction.py
@@ -16,6 +16,15 @@ PROPANE_CONVERSION_FACTOR = 138.63
 WOOD_CONVERSION_FACTOR = 2.5*116.65
 SOME_OTHER_FUEL_CONVERSION_FACTOR = 116.65
 
+Conversion_Factors = [ ["Electricity", ELECTRICITY_CONVERSION_FACTOR], ["Natural gas", NATURAL_GAS_CONVERSION_FACTOR], ["Fuel oil or kerosone", FUEL_OIL_OR_KEROSENE_CONVERSION_FACTOR], ["Propane", PROPANE_CONVERSION_FACTOR], ["Wood", WOOD_CONVERSION_FACTOR], ["Some other fuel", SOME_OTHER_FUEL_CONVERSION_FACTOR]]
+
+SH_HOURS_PER_DAY = 8
+WH_HOURS_PER_DAY = 4
+AC_HOURS_PER_DAY = 35/60 * 14
+REFRIGIERATORS_HOURS_PER_DAY = 8
+OTHER_CONSUMPTION_HOURS_PER_DAY = 8
+
+Consumption_Hours = [SH_HOURS_PER_DAY, WH_HOURS_PER_DAY, AC_HOURS_PER_DAY, REFRIGERATORS_HOURS_PER_DAY, OTHER_CONSUMPTION_HOURS_PER_DAY] 
 
 housing_unit_type = input("Housing Unit Type: ")
 ownership = input("Ownership: ")
@@ -27,10 +36,13 @@ payment_method = input("Payment Method: ")
 
 statsArray = [["Housing unit type", housing_unit_type], ["Ownership of housing unit", ownership], ["Year of construction", year_of_construction], ["Total square footage", square_footage], ["Number of household members", num_of_household_members], ["2020 annual household income", household_income], ["Payment method for energy bills", payment_method]]
 
-arrayOfFuels =  [ ["Space heating", "", 0 ],  ["Water heating", "", 0 ], ["Air conditioning", "", 0 ], ["Refrigerators", "", 0 ], ["Other", "", 0 ]] 
+arrayOfFuels =  [ ["Space heating", "", 1 ],  ["Water heating", "", 1 ], ["Air conditioning", "", 1], ["Refrigerators", "", 1 ], ["Other", "", 1 ]] 
+symbolsOfFuels = [sh_consumption, wh_consumption, ac_consumption, refrigerators_consumption, other_consumption]
+
 
 coefficient = 1
-func = coefficient* (sh_consumption * sh_time + wh_consumption * wh_time + ac_consumption * ac_time + refrigerators_consumption * refrigerators_time + other_consumption*other_time)
+func = coefficient*(sh_consumption * sh_time + wh_consumption * wh_time + ac_consumption * ac_time + refrigerators_consumption * refrigerators_time + other_consumption*other_time)
+var = func
 
 for fuel in arrayOfFuels:
   fuel[1] = input("Type of Fuel for " + fuel[0] + ": ") 
@@ -44,6 +56,17 @@ for i in range(statsArray):
   
   factor = average_energy_site_consumption/TOTAL_CONSUMPTION_PER_HOUSE
   coefficient = coefficient*factor
+
+for i in range(len(arrayOfFuels)):
+  fuel = arrayOfFuels[i]
+  average_energy_site_consumption = 1
+  summand = (average_energy_site_consumption/(Consumption_Hours[i]))
+  for factor in Conversion_Factors:
+    if (fuel[1] == factor[0]):
+      summand = summand*factor[1]
+  var = var.subs(symbolsOfFuels[i], summand)
+    
+  
 
  
   
