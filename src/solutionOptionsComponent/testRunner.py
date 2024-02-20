f3ab3afd613eb2596bbdaeab51b8b7b10ba5918e
InfinityImperium <123785084+InfinityImperium@users.noreply.github.com>
2024-02-19 21:33:00 -0600

    Update testRunner.py

--- src/solutionOptionsComponent/testRunner.py
+++ src/solutionOptionsComponent/testRunner.py
@@ -5,6 +5,7 @@ import sys
 import os
 import determineFunction as *
 import evaluateEmissions as *
+sh_consumption, sh_time, wh_consumption, wh_time, ac_consumption, ac_time, refrigerators_consumption, refrigerators_time, other_consumption, other_time = symbols("sh_consumption sh_time wh_consumption wh_time ac_consumption ac_time refrigerators_consumption refrigerators_time other_consumption other_time")
 
 determineFunction("dataFiles/current_user_data.json")
 print(evaluateEmissions("dataFiles/current_user_data.json", 12)) 
