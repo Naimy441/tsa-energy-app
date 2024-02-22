e9dfb461b2fba2e2bc4e9b176277f02e90815251
InfinityImperium <123785084+InfinityImperium@users.noreply.github.com>
2024-02-21 22:53:01 -0600

    Update testRunner.py

--- src/solutionOptionsComponent/testRunner.py
+++ src/solutionOptionsComponent/testRunner.py
@@ -1,10 +1,16 @@
-import sympy as *
+from sympy import *
 import pandas as pd
 import json
 import sys
 import os
-import determineFunction as *
-import evaluateEmissions as *
+from determineFunction import *
+from evaluateEmissions import *
+import os
+
+#cwd = os.getcwd()  # Get the current working directory (cwd)
+#files = os.listdir(cwd)  # Get all the files in that directory
+#print("Files in %r: %s" % (cwd, files))
+
 sh_consumption, sh_time, wh_consumption, wh_time, ac_consumption, ac_time, refrigerators_consumption, refrigerators_time, other_consumption, other_time = symbols("sh_consumption sh_time wh_consumption wh_time ac_consumption ac_time refrigerators_consumption refrigerators_time other_consumption other_time")
 
 determineFunction("dataFiles/current_user_data.json")
