85cc2e6ce537da2aee3863613434420d573abe8e
InfinityImperium <123785084+InfinityImperium@users.noreply.github.com>
2024-02-22 23:16:31 -0600

    Update testRunner.py

--- src/solutionOptionsComponent/testRunner.py
+++ src/solutionOptionsComponent/testRunner.py
@@ -14,4 +14,8 @@ import os
 sh_consumption, sh_time, wh_consumption, wh_time, ac_consumption, ac_time, refrigerators_consumption, refrigerators_time, other_consumption, other_time = symbols("sh_consumption sh_time wh_consumption wh_time ac_consumption ac_time refrigerators_consumption refrigerators_time other_consumption other_time")
 
 determineFunction("dataFiles/current_user_data.json")
-print(evaluateEmissions("dataFiles/current_user_data.json", 12)) 
+
+list = [] 
+for (t in range(60)):
+  list.append([t, evaluateEmissions("dataFiles/current_user_data.json", 12)])
+
