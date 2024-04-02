743f1a75a75707f9daecb08d49952df5f150c254
InfinityImperium <123785084+InfinityImperium@users.noreply.github.com>
2024-04-02 14:50:14 -0500

    Create sinusoidalFit.py

---
+++ src/solutionOptionsComponent/sinusoidalFit.py
@@ -0,0 +1,63 @@
+from sympy import *
+
+def sinusoidalFit():
+  DICTIONARY = {
+    "January": 1/12,
+    "February": 2/12,
+    "March": 3/12,
+    "April": 4/12,
+    "May": 5/12,
+    "June": 6/12,
+    "July": 7/12,
+    "August": 8/12,
+    "September": 9/12,
+    "October": 10/12,
+    "November": 11/12,
+    "December": 1
+  }
+
+
+  t = symbols("t")
+  
+  with open("dataFiles/current_user_data.json", "r") as user_data:
+    jsonUserData = json.load(user_data)
+
+
+  #SPACE HEATING
+  maxshtime = jsonUserData["Consumption information"]["Space heating"][1][1]
+  maxshmonth = jsonUserData["Consumption information"]["Space heating"][1][0]
+  maxshmonth = DICTIONARY[maxshmonth]
+
+  minshtime = jsonUserData["Consumption information"]["Space heating"][2][1]
+  minshmonth = jsonUserData["Consumption information"]["Space heating"][2][0]
+  minshmonth = DICTIONARY[minshmonth]
+
+  averagesh = (maxshtime + minshtime) / 2
+
+  totalshtime = (maxshtime + minshtime)/2 + sin( ) 
+
+  
+
+
+  #WATER HEATING
+  maxwhtime = jsonUserData["Consumption information"]["Water heating"][1][1]
+  maxwhmonth = jsonUserData["Consumption information"]["Water heating"][1][0]
+  maxwhmonth = DICTIONARY[maxwhmonth]
+  
+  minwhtime = jsonUserData["Consumption information"]["Water heating"][2][1]
+  minwhmonth = jsonUserData["Consumption information"]["Water heating"][2][0]
+  minwhmonth = DICTIONARY[minwhmonth]
+
+
+  #AIR CONDITIONING
+  maxactime = jsonUserData["Consumption information"]["Air conditioning"][1][1]
+  maxacmonth = jsonUserData["Consumption information"]["Air conditioning"][1][0]
+  maxacmonth = DICTIONARY[maxacmonth]
+  
+  minactime = jsonUserData["Consumption information"]["Air conditioning"][2][1]
+  minacmonth = jsonUserData["Consumption information"]["Air conditioning"][2][0]
+  minacmonth = DICTIONARY[minacmonth]
+  
+  
+
+  
