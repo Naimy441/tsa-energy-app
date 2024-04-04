c516e03129c12d5cf5ca1ff1034e54c610c80387
InfinityImperium <123785084+InfinityImperium@users.noreply.github.com>
2024-04-03 23:51:36 -0500

    Update testRunner.py

--- src/solutionOptionsComponent/testRunner.py
+++ src/solutionOptionsComponent/testRunner.py
@@ -16,13 +16,13 @@ for i in range(60):
     array1.append([i, jsonUserData["CO2 Function"].subs(t, i/12)])
     array2.append([i, jsonUserData["CO2 Accumulation Function"].subs(x, i/12)])
 
-  with open ("dataFiles/current_user_data.json", "r") as f:
+with open ("dataFiles/current_user_data.json", "r") as f:
     jsonData = json.load(f)
     jsonData["ipcData1"] = array1
     jsonData["ipcData2"] = array2
     newData  = json.dumps(jsonData, indent = 4)
 
-  with open("dataFiles/current_user_data.json", "w") as file2:
+with open("dataFiles/current_user_data.json", "w") as file2:
     file2.write(newData)
 
 
