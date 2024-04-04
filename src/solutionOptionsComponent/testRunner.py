ba2e646b6844e393008fd2dfb7f2c7df9876b19b
InfinityImperium <123785084+InfinityImperium@users.noreply.github.com>
2024-04-03 23:31:03 -0500

    Update testRunner.py

--- src/solutionOptionsComponent/testRunner.py
+++ src/solutionOptionsComponent/testRunner.py
@@ -1,5 +1,6 @@
 import json
 from sympy as *
+from determineFunction import determineFunction
 
 with open("dataFiles/current_user_data.json", "r") as user_data:
     jsonUserData = json.load(user_data)
