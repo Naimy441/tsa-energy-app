ec9ad83d9ad216ade5457992346d7631e2ef6436
InfinityImperium <123785084+InfinityImperium@users.noreply.github.com>
2024-04-03 23:44:59 -0500

    Update testRunner.py

--- src/solutionOptionsComponent/testRunner.py
+++ src/solutionOptionsComponent/testRunner.py
@@ -2,5 +2,28 @@ import json
 from sympy as *
 from determineFunction import determineFunction
 
+x,t = symbols("x t")
+
+determineFunction()
+
 with open("dataFiles/current_user_data.json", "r") as user_data:
     jsonUserData = json.load(user_data)
+
+array1 = jsonUserData["ipcData1"]
+array2 = jsonUserData["ipcData2"]
+
+for i in range(60):
+    array1.append([i, jsonUserData["CO2 Function"].subs(t, i/12)])
+    array2.append([i, jsonUserData["CO2 Accumulation Function"].subs(x, i/12)])
+
+  with open ("dataFiles/current_user_data.json", "r") as f:
+    jsonData = json.load(f)
+    jsonData["ipcData1"] = array1
+    jsonData["ipcData2"] = array2
+    newData  = json.dumps(jsonData, indent = 4)
+
+  with open("dataFiles/current_user_data.json", "w") as file2:
+    file2.write(newData)
+
+
+    
